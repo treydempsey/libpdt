cb8aeff7db39eccd484dc8062a6812f59e7206d0
Trey Dempsey <trey.dempsey@gmail.com>
2010-10-23 12:17:48 -0500

    Completed tests and added copyright notices

--- src/class_String.h
+++ src/class_String.h
@@ -1,3 +1,26 @@
+/*
+ *  Copyright 2010  Trey Dempsey
+ *
+ *  libparse_delimited_text is distributed under the terms of the GNU
+ *  Lesser General Public License.
+ * 
+ *  This file is part of libparse_delimited_test.
+ *
+ *  libparse_delimited_test is free software: you can redistribute it
+ *  and/or modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation, either
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  libparse_delimited_test is distributed in the hope that it will be
+ *  useful, but WITHOUT ANY WARRANTY; without even the implied
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with libparse_delimited_text.  If not, see
+ *  <http://www.gnu.org/licenses/>.
+ ***********************************************************************/
+
 #ifndef _CLASS_STRING_H_
 #define _CLASS_STRING_H_
 
